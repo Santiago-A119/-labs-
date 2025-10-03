423ee640ce56dfe8f580f0c060fc581a0c6f7b00
Santiago-A119 <santiagoagredo940@gmail.com>
2025-10-03 20:37:15 +0300

    animacion.py

--- animacion.py
+++ animacion.py
@@ -2,11 +2,9 @@
 import os, time
 
 def clear_console():
-    """Clears the console screen."""
     os.system("cls" if os.name == "nt" else "clear")
 
 def animate_three_frames(duration_sec=5):
-    """Runs a simple 3-frame animation."""
     frames = ["(•  )", "( • )", "(  •)"]
     t0 = time.time()
     i = 0
@@ -19,3 +17,4 @@ def animate_three_frames(duration_sec=5):
 
 if __name__ == "__main__":
     animate_three_frames()
+
